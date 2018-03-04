cfa78741cacf6772eb21caba952ed1f294fda204
ruchitgandhi <gemini.ruchit@gmail.com>
2018-03-04 09:24:40 -0800

    Adding Integration

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get('/', function (req, res, next) {
   res.render('index',
     { title: 'Let\'s check out how much food is wasted everyday?',
-      description: 'Enter the feed for the wasted plate and the person responsible for it' })
+      description: 'Enter the image url for the wasted plate and the person responsible for it' })
 })
 
 module.exports = router
